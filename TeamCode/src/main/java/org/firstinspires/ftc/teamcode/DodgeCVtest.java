57c37c022d2cd1d7d1902e92c3263da643c095bc
chrisb523 <43218982+chrisb523@users.noreply.github.com>
2018-12-04 15:58:33 -0500

    Fix dodge cv and 1000 errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DodgeCVtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DodgeCVtest.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 import org.firstinspires.ftc.DodgeCV.*;
 public class DodgeCVtest {
+    @TeleOp(name="GoldAlign Example", group="DogeCV")
+
+    public class GoldAlignExample extends OpMode
+    {
+        // Detector object
+        private GoldAlignDetector detector;
 
 }
