444cae8f1c484b94ce2d146fdf8453b723e4ad97
CouchWafer <alex_dietrich@caryacademy.org>
2018-12-13 16:37:44 -0500

    DodgeCV implemented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -22,6 +25,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
         private DcMotor intake = null;
         private DcMotor intakeArm = null;
         private Servo linearServo = null;
+
+        //Declares detector
+        private GoldAlignDetector detector;
 
         @Override
         public void runOpMode()
@@ -82,6 +88,44 @@ import com.qualcomm.robotcore.util.ElapsedTime;
             frontLeft.setPower(0);
             frontRight.setPower(0);
             backLeft.setPower(0);
+
+            /*
+             * DodgeCV portion
+             * Scanners: Gold Align Detector
+             */
+
+            telemetry.addData("Status", "DogeCV 2018.0 - Gold Align Example");
+
+            // Set up detector
+            detector = new GoldAlignDetector(); // Create detector
+            detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
+            detector.useDefaults(); // Set detector to use default settings
+
+            // Optional tuning
+            detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+            detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+            detector.downscale = 0.4; // How much to downscale the input frames
+
+            detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+            //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+            detector.maxAreaScorer.weight = 0.005; //
+
+            detector.ratioScorer.weight = 5; //
+            detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+
+            detector.enable(); // Start the detector!
+
+            /*
+             * Check if the Robot is aligned
+             * And Position
+             */
+            while (opModeIsActive() && (runtime.seconds() < 27)) {
+                telemetry.addData("IsAligned", detector.getAligned()); // Is the bot aligned with the gold mineral?
+                telemetry.addData("X Pos", detector.getXPosition()); // Gold X position.
+
+                // Disable the detector
+                detector.disable();
+            }
         }
 }
 
