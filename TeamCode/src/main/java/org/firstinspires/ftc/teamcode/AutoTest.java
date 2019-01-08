a7eecade909ac806feba43b06635031c2606ba9b
chrisb523 <43218982+chrisb523@users.noreply.github.com>
2019-01-08 16:05:23 -0500

    large dab

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -12,8 +12,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @Autonomous(name="mecanumAutonomous", group="Mecanum")
-    public class AutoTest extends LinearOpMode
-{
+    public class AutoTest extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeft = null;
@@ -25,129 +24,153 @@ import com.qualcomm.robotcore.util.ElapsedTime;
     private DcMotor intake = null;
     private DcMotor intakeArm = null;
 
-        //Declares detector
-        private GoldAlignDetector detector;
+    //Declares detector
+    private GoldAlignDetector detector;
 
-        @Override
-        public void runOpMode()
-        {
-            telemetry.addData("Status", "Initialized");
-            telemetry.update();
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-            frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
-            frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-            backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-            backRight  = hardwareMap.get(DcMotor.class, "backRight");
-            lift = hardwareMap.get(DcMotor.class, "lift");
-            armExtension = hardwareMap.get(DcMotor.class, "armExtension");
-            intake = hardwareMap.get(DcMotor.class, "intake");
-            intakeArm = hardwareMap.get(DcMotor.class, "intakeArm");
-            // Most robots need the motor on one side to be reversed to drive forward
-            // Reverse the motor that runs backwards when connected directly to the battery
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        armExtension = hardwareMap.get(DcMotor.class, "armExtension");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        intakeArm = hardwareMap.get(DcMotor.class, "intakeArm");
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
 
-            frontLeft.setDirection(DcMotor.Direction.REVERSE);
-            backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
 
-            // Tell the driver that initialization is complete.
-            telemetry.addData("Status", "Initialized");
-                waitForStart();
-            lift.setPower(1);
-            runtime.reset();
-            while (opModeIsActive() && (runtime.seconds() < 6.6))
-            {
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+        waitForStart();
+        lift.setPower(1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 6.6)) {
             telemetry.addData("Path", "Lowering", runtime.seconds());
             telemetry.update();
-            }
-            lift.setPower(0);
-
-            runtime.reset();
-            backRight.setPower(-.5);
-            frontLeft.setPower(.5);
-            frontRight.setPower(.5);
-            backLeft.setPower(-.5);
-            while (opModeIsActive() && (runtime.seconds() < 1))
-            {
-                telemetry.addData("Path", "Lowering", runtime.seconds());
-                telemetry.update();
-            }
-            runtime.reset();
-
-            backRight.setPower(.5);
-            frontLeft.setPower(.5);
-            frontRight.setPower(.5);
-            backLeft.setPower(.5);
-            while (opModeIsActive() && (runtime.seconds() < 1.5))
-            {
-                telemetry.addData("Path", "Lowering", runtime.seconds());
-                telemetry.update();
-            }
-
-            backRight.setPower(0);
-            frontLeft.setPower(0);
-            frontRight.setPower(0);
-            backLeft.setPower(0);
-
-            /*
-             * DodgeCV portion
-             * Scanners: Gold Align Detector
-             */
-
-            telemetry.addData("Status", "DogeCV 2018.0 - Gold Align Example");
-
-            // Set up detector
-            detector = new GoldAlignDetector(); // Create detector
-            detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
-            detector.useDefaults(); // Set detector to use default settings
-
-            // Optional tuning
-            detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
-            detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
-            detector.downscale = 0.4; // How much to downscale the input frames
-
-            detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
-            //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
-            detector.maxAreaScorer.weight = 0.005; //
-
-            detector.ratioScorer.weight = 5; //
-            detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
-
-            detector.enable(); // Start the detector!
-
-            /*
-             * Check if the Robot is aligned
-             * And Position
-             */
-            while (opModeIsActive() && (runtime.seconds() < 27)) {
-                if(detector.isFound() == false) {
-                        runtime.reset();
-                        backRight.setPower(1);
-                        backLeft.setPower(-1);
-                        frontRight.setPower(1);
-                        frontLeft.setPower(-1);
-                        while (opModeIsActive() && (runtime.seconds() < .5) && detector.isFound() ==false) {
-                            telemetry.addData("Path", "Locating Gold", runtime.seconds());
-                            telemetry.update();
-                        }
-                        runtime.reset();
-                        backRight.setPower(-1);
-                        backLeft.setPower(1);
-                        frontRight.setPower(-1);
-                        frontLeft.setPower(1);
-                        while (opModeIsActive() && (runtime.seconds() < 1) && detector.isFound() ==false) {
-                            telemetry.addData("Path", "Locating Gold", runtime.seconds());
-                            telemetry.update();
-                        }
-
-                }
-                telemetry.addData("Block Status:", "Located", runtime.seconds());
-                //code for moving the robot to find gold blocks
-            }
-            //General code for knocking block (Lower Arm and turn robot to knock the block
-
-                // Disable the detector
-                detector.disable();
-
         }
+        lift.setPower(0);
+
+        runtime.reset();
+        backRight.setPower(-.5);
+        frontLeft.setPower(.5);
+        frontRight.setPower(.5);
+        backLeft.setPower(-.5);
+        while (opModeIsActive() && (runtime.seconds() < 1)) {
+            telemetry.addData("Path", "Lowering", runtime.seconds());
+            telemetry.update();
+        }
+        runtime.reset();
+
+        backRight.setPower(.5);
+        frontLeft.setPower(.5);
+        frontRight.setPower(.5);
+        backLeft.setPower(.5);
+        while (opModeIsActive() && (runtime.seconds() < 1.5)) {
+            telemetry.addData("Path", "Lowering", runtime.seconds());
+            telemetry.update();
+        }
+
+        backRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+
+        /*
+         * DodgeCV portion
+         * Scanners: Gold Align Detector
+         */
+
+        telemetry.addData("Status", "DogeCV 2018.0 - Gold Align Example");
+
+        // Set up detector
+        detector = new GoldAlignDetector(); // Create detector
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
+        detector.useDefaults(); // Set detector to use default settings
+
+        // Optional tuning
+        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.4; // How much to downscale the input frames
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        detector.maxAreaScorer.weight = 0.005; //
+
+        detector.ratioScorer.weight = 5; //
+        detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+
+        detector.enable(); // Start the detector!
+
+        /*
+         * Check if the Robot is aligned
+         * And Position
+         */
+        while (opModeIsActive() && (runtime.seconds() < 27)) {
+            if (detector.isFound() == false) {
+                runtime.reset();
+                backRight.setPower(1);
+                backLeft.setPower(-1);
+                frontRight.setPower(1);
+                frontLeft.setPower(-1);
+                while (opModeIsActive() && (runtime.seconds() < .5) && detector.isFound() == false) {
+                    telemetry.addData("Block Status:", "Locating Gold", runtime.seconds());
+                    telemetry.update();
+                }
+                runtime.reset();
+                backRight.setPower(-1);
+                backLeft.setPower(1);
+                frontRight.setPower(-1);
+                frontLeft.setPower(1);
+                while (opModeIsActive() && (runtime.seconds() < 1) && detector.isFound() == false) {
+                    telemetry.addData("Block Status:", "Locating Gold", runtime.seconds());
+                    telemetry.update();
+                }
+            }
+            telemetry.addData("Block Status:", "Located", runtime.seconds());
+            //code for aligning the robot
+            if (detector.getAligned() == false) {
+                runtime.reset();
+                backRight.setPower(1);
+                backLeft.setPower(-1);
+                frontRight.setPower(1);
+                frontLeft.setPower(-1);
+                while (opModeIsActive() && (runtime.seconds() < .25) && detector.getAligned() == false) {
+                    telemetry.addData("Block Status:", "Aligning Gold", runtime.seconds());
+                    telemetry.update();
+                }
+                runtime.reset();
+                backRight.setPower(-1);
+                backLeft.setPower(1);
+                frontRight.setPower(-1);
+                frontLeft.setPower(1);
+                while (opModeIsActive() && (runtime.seconds() < .5) && detector.getAligned() == false) {
+                    telemetry.addData("Block Status:", "Aligning Gold", runtime.seconds());
+                    telemetry.update();
+                }
+            }
+            //code to knock block
+            runtime.reset();
+            backRight.setPower(1);
+            backLeft.setPower(1);
+            frontRight.setPower(1);
+            frontLeft.setPower(1);
+            while (opModeIsActive() && (runtime.seconds() < 1)) {
+                telemetry.addData("Block Status:", "Knocking gold", runtime.seconds());
+                telemetry.update();
+            }
+            // Disable the detector
+            detector.disable();
+            break;
+        }
+
+    }
 }
 
 
